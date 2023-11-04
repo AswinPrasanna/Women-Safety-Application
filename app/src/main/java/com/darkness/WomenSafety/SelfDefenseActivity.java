9bce88a9efb61b0be911c7954d786193b1c34024
Aswin Prasanna <136261716+AswinPrasanna@users.noreply.github.com>
2023-11-04 09:01:53 +0530

    Add files via upload

---
+++ app/src/main/java/com/darkness/WomenSafety/SelfDefenseActivity.java
@@ -0,0 +1,28 @@
+package com.darkness.WomenSafety;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.webkit.WebChromeClient;
+import android.webkit.WebView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SelfDefenseActivity extends AppCompatActivity {
+
+    WebView webView;
+
+    @SuppressLint("SetJavaScriptEnabled")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_self_defense);
+
+        webView = findViewById(R.id.webView);
+
+        webView.setWebChromeClient(new WebChromeClient());
+        webView.getSettings().setLoadsImagesAutomatically(true);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.loadUrl("https://www.youtube.com/embed/T7aNSRoDCmg");
+
+    }
+}
